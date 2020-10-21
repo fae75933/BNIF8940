8e43e674480f5b240e1b23d6459810d751889678
fae75933 <69994033+fae75933@users.noreply.github.com>
2020-10-20 22:36:28 -0400

    updates

--- Homework1.sh
+++ Homework1.sh
@@ -2,14 +2,18 @@
 #SBATCH --job-name=homework1
 #SBATCH --partition=batch
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=6
 #SBATCH --mem=10gb
 #SBATCH --time=2:00:00
 #SBATCH --output=/work/BNIF8940/fae75933/log.%j
 #SBATCH --mail-user=fae75933@uga.edu
 #SBATCH --mail-type=END,FAIL
 
-OUTDIR="/work/gene8940/fae75933/"
+OUTDIR="/work/gene8940/fae75933/homework1"
+if [ ! -d $OUTDIR ]
+then
+    mkdir -p $OUTDIR
+fi
 cd $OUTDIR
 curl -s ftp://ftp.ensemblgenomes.org/pub/bacteria/release-37/gff3/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655/Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.37.gff3.gz | gunzip -c > $OUTDIR/ecoli_MG1655.gff
-grep -c "CDS" ecoli_MG1655
+grep -c "CDS" ecoli_MG1655.GFF> $OUTDIR
