0e3aa10b56fa5690dece4be6be55ab1d8643441c
fae75933 <69994033+fae75933@users.noreply.github.com>
2020-12-05 22:45:43 -0500

    Update cutrun.sh

--- cutrun.sh
+++ cutrun.sh
@@ -67,8 +67,7 @@ bigwig="$OUTDIR/Bigwigs/$file.bw"
 
 
 ##map reads and convert to sorted bam file. This is a bwa command, then output is piped to "samtools view", them this output is piped to "samtools sort"
-bwa mem -M -v 3 -t $THREADS /scratch/fae75933/genomesfolder/GCA_000182925_neurospora.fna $f $read2 | samtools view -bhu - | samtools sort -@ $THREADS -T $file -o
- "$sorted.bam" -O bam -
+bwa mem -M -v 3 -t $THREADS /scratch/fae75933/genomesfolder/GCA_000182925_neurospora.fna $f $read2 | samtools view -bhu - | samtools sort -@ $THREADS -T $file -o "$sorted.bam" -O bam -
 
 samtools index "$sorted.bam"
 
